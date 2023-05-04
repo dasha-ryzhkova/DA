04fff565d2d0dc78a6d35f0214e0a9c46ae10c0e
dasha-ryzhkova <ryzhkovadasha2002@gmail.com>
2023-05-04 20:47:20 +0300

    add log() plots

--- TOP-15/All_countrys.R
+++ TOP-15/All_countrys.R
@@ -26,6 +26,16 @@ ggplot(relative_data, aes(fill = hotel, x = country, y = percentage)) +
         legend.text = element_text(size = 10))
 relative_data
 
+ggplot(relative_data, aes(fill = hotel, x = country, y = log(n))) +
+  geom_bar(position="stack", stat="identity")+
+  labs(x = "Тип готелю", y = "log(Бронювання)") +
+  scale_x_discrete(labels = c(unique(relative_data[, 'country']))) +
+  scale_fill_discrete(labels = c("Resort Hotel", "City Hotel", fill='Hotel')) +
+  theme(axis.title = element_text(size = 10),
+        axis.text = element_text(size = 10),
+        legend.title = element_text(size = 10),
+        legend.text = element_text(size = 10))
+
 ggplot(relative_data, aes(fill = hotel, x = country, y = n)) +
   geom_bar(position="stack", stat="identity")+
   labs(x = "Тип готелю", y = "Бронювання") +
@@ -35,6 +45,7 @@ ggplot(relative_data, aes(fill = hotel, x = country, y = n)) +
         axis.text = element_text(size = 10),
         legend.title = element_text(size = 10),
         legend.text = element_text(size = 10))
+
 
 # для data9
 top_countries_list <- data9 %>%
@@ -56,6 +67,16 @@ relative_data <- top_countries %>%
 ggplot(relative_data, aes(fill = hotel, x = country, y = percentage)) +
   geom_bar(position="stack", stat="identity")+
   labs(x = "Тип готелю", y = "Відносна частка бронювань (%)") +
+  scale_x_discrete(labels = c(unique(relative_data[, 'country']))) +
+  scale_fill_discrete(labels = c("Resort Hotel", "City Hotel", fill='Hotel')) +
+  theme(axis.title = element_text(size = 10),
+        axis.text = element_text(size = 10),
+        legend.title = element_text(size = 10),
+        legend.text = element_text(size = 10))
+
+ggplot(relative_data, aes(fill = hotel, x = country, y = log(n))) +
+  geom_bar(position="stack", stat="identity")+
+  labs(x = "Тип готелю", y = "log(Бронювання)") +
   scale_x_discrete(labels = c(unique(relative_data[, 'country']))) +
   scale_fill_discrete(labels = c("Resort Hotel", "City Hotel", fill='Hotel')) +
   theme(axis.title = element_text(size = 10),
